fa6f47af56be45d980c1315c6e575fad0994c147
AmaniFad <amani.fad12@gmail.com>
2025-02-17 22:00:22 +0000

    mimosa echa

--- Assets/Scripts/Plants/MimosaPlant.cs
+++ Assets/Scripts/Plants/MimosaPlant.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class MimosaPlant : MonoBehaviour
 {
     [SerializeField] private float durationBox;
     [SerializeField] private float finalTargerX;
+
+
+    [SerializeField] private List<BoxCollider> boxList = new List<BoxCollider>();
 
     private BoxCollider boxCollider;
     private float initialSizeX;
@@ -11,6 +16,12 @@ public class MimosaPlant : MonoBehaviour
     private float duration;
     private float elapsedTime = 0f;
     private bool isReducing = false;
+
+    [SerializeField] private float timeBetweenCollider;
+    private int i;
+    private float coolDown = 0;
+    private bool isCooldown;
+    private bool isAllBox;
 
     void Start()
     {
@@ -21,30 +32,27 @@ public class MimosaPlant : MonoBehaviour
             return;
         }
         initialSizeX = boxCollider.size.x;
+
+        i = 0;
     }
 
     void Update()
     {
-        if (isReducing)
+        if (isReducing & !isAllBox)
         {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / duration);
-
-            // Reducir el tamaño desde la derecha hacia la izquierda
-            float newSizeX = Mathf.Lerp(initialSizeX, targetSizeX, t);
-            float sizeDifference = initialSizeX - newSizeX;
-
-            Vector3 newSize = boxCollider.size;
-            newSize.x = newSizeX;
-
-            Vector3 newCenter = boxCollider.center;
-            newCenter.x -= sizeDifference / 2;  // Desplazar el centro para cerrar de derecha a izquierda
-
-            boxCollider.size = newSize;
-            boxCollider.center = newCenter;
-
-            if (t >= 1f) isReducing = false;
+            //reducieCollider();
+            coolDown -= Time.deltaTime;
+            if (i < boxList.Count)
+            {
+                reduceListCollider();
+            }
+            else
+            {
+                isReducing = false;
+                isAllBox = true;
+            }
         }
+        
     }
 
     public void StartReducing(float newTargetSizeX, float newDuration)
@@ -61,7 +69,46 @@ public class MimosaPlant : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             // Iniciar la reducción del BoxCollider
-            StartReducing(finalTargerX, durationBox);
+            //StartReducing(finalTargerX, durationBox);
+            isReducing = true;
         }
+    }
+    private void reduceListCollider()
+    {
+        if (!isCooldown)
+        {
+            if (coolDown <= 0)
+            {
+                boxList[i].enabled = false;
+                i++;
+
+                isCooldown = true;
+                coolDown = timeBetweenCollider;
+            }
+        }
+        else
+        {
+            isCooldown = false;
+        }
+    }
+    private void reducieCollider()
+    {
+        elapsedTime += Time.deltaTime;
+        float t = Mathf.Clamp01(elapsedTime / duration);
+
+        // Reducir el tamaño desde la derecha hacia la izquierda
+        float newSizeX = Mathf.Lerp(initialSizeX, targetSizeX, t);
+        float sizeDifference = initialSizeX - newSizeX;
+
+        Vector3 newSize = boxCollider.size;
+        newSize.x = newSizeX;
+
+        Vector3 newCenter = boxCollider.center;
+        newCenter.x -= sizeDifference / 2;  // Desplazar el centro para cerrar de derecha a izquierda
+
+        boxCollider.size = newSize;
+        boxCollider.center = newCenter;
+
+        if (t >= 1f) isReducing = false;
     }
 }

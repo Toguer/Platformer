899dd5c15a5a39cd6b660b6abb5b8fc9e77367f5
Zoe <a23zoeguepar@inspedralbes.cat>
2025-02-25 11:35:54 +0100

    https://trello.com/c/KwZNSmoE/28-arreglar-movimiento-con-el-mando

--- Assets/InputSystem_Actions.cs
+++ Assets/InputSystem_Actions.cs
@@ -302,7 +302,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""daba33a1-ad0c-4742-a909-43ad1cdfbeb6"",
-                    ""path"": ""<Gamepad>/rightTrigger"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Gamepad"",

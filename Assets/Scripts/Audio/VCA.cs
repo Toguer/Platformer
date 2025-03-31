d51046367d3a3b03ea9cb6d4033eeaab8a2ea79b
Meldarse <meldarse@gmail.com>
2025-03-31 12:14:28 -0500

    Reapply "Merge branch 'Zoe' into ArteXAmaniXZoe"
    This reverts commit bb7dc9eba963767a149121fd432775f4effc3bc7.

--- Assets/Scripts/Audio/VCA.cs
+++ Assets/Scripts/Audio/VCA.cs
@@ -3,19 +3,33 @@ using UnityEngine.Serialization;
 
 public class VCA : MonoBehaviour
 {
-    private FMOD.Studio.VCA _vca;
-    [SerializeField] [Range(-80f, 10f)]
-    private float _vcaVolume;
+    private FMOD.Studio.VCA _musicVca;
+    [SerializeField] [Range(-80f, 10f)] private float _musicVolume;
+    private FMOD.Studio.VCA _sfxVca;
+    [SerializeField] [Range(-80f, 10f)] private float _sfxVolume;
+    private FMOD.Studio.VCA _uiVca;
+    [SerializeField] [Range(-80f, 10f)] private float _uiVolume;
+
+    [Tooltip("El formato es \"vca:/Music\"")] [SerializeField]
+    private string _musicPath;
+
+    [SerializeField] private string _sfxPath;
+    [SerializeField] private string _uiPath;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _vca = FMODUnity.RuntimeManager.GetVCA("vca:/Music");
+        _musicVca = FMODUnity.RuntimeManager.GetVCA(_musicPath);
+        _sfxVca = FMODUnity.RuntimeManager.GetVCA(_sfxPath);
+        _uiVca = FMODUnity.RuntimeManager.GetVCA(_uiPath);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _vca.setVolume(DecibelToLinear(_vcaVolume));
+        _musicVca.setVolume(DecibelToLinear(_musicVolume));
+        _sfxVca.setVolume(DecibelToLinear(_sfxVolume));
+        _uiVca.setVolume(DecibelToLinear(_uiVolume));
     }
 
     private float DecibelToLinear(float dB)
